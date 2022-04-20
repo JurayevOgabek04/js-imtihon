61c1db452fb5616d02b44f3ed0eee867527fce98
JurayevOgabek04 <jurayevogabek0304>
2022-04-20 16:32:07 +0500

    Imtihon

---
+++ JS/js_imtixon.js
@@ -0,0 +1,26 @@
+//  Hotamov Og'abek 16:00
+
+
+ // 1
+
+var arr = ["I","study","JavaScript","rigth","now"]
+
+arr.splice(2,1)
+console.log(arr);
+
+
+
+//   // 3
+
+// let num = +prompt("")
+// function Juft_toq(num) {
+//     if (num % 2 === 0){
+//         return true
+//     } else {
+//         return  false
+//     } 
+// }
+
+// let number = Juft_toq(num)
+
+// console.log(number);
